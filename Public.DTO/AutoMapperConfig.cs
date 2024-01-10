1d12c35fd1e954e955afddcec1db34402deac5a5
Andree Uuetoa <andree.uuetoa@gmail.com>
2024-01-10 18:44:54 +0200

    Add room, competition and concert mapping to public DTO automapperconfig

--- Public.DTO/AutoMapperConfig.cs
+++ Public.DTO/AutoMapperConfig.cs
@@ -10,5 +10,8 @@ public class AutoMapperConfig : Profile
         CreateMap<BLL.DTO.Library.BookLentOut, v1._0.Library.BookLentOut>().ReverseMap();
         CreateMap<BLL.DTO.Identity.AppUser, v1._0.Identity.AppUser>().ReverseMap();
         CreateMap<BLL.DTO.Identity.AppRole, v1._0.Identity.AppRole>().ReverseMap();
+        CreateMap<BLL.DTO.Competitions.Competition, v1._0.Competitions.Competition>().ReverseMap();
+        CreateMap<BLL.DTO.Concerts.Concert, v1._0.Concerts.Concert>().ReverseMap();
+        CreateMap<BLL.DTO.Rooms.Room, v1._0.Rooms.Room>().ReverseMap();
     }
 }
