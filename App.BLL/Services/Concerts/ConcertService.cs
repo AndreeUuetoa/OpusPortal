bcd3ccafc5c31f742fda6d85ce961745ca8ade44
Andree Uuetoa <andree.uuetoa@gmail.com>
2024-01-10 15:33:15 +0200

    Nicer names for mapper classes in concert service

--- App.BLL/Services/Concerts/ConcertService.cs
+++ App.BLL/Services/Concerts/ConcertService.cs
@@ -3,7 +3,6 @@ using App.DAL.Contracts;
 using App.DAL.Contracts.Repositories.Concerts;
 using Base;
 using Base.BLL;
-using Domain.Concerts;
 using DomainConcert = Domain.Concerts.Concert;
 using BLLConcert = BLL.DTO.Concerts.Concert;
 
@@ -13,7 +12,7 @@ public class ConcertService : BaseEntityService<DomainConcert, BLLConcert, IConc
 {
     private readonly IAppUOW _uow;
     
-    public ConcertService(IAppUOW uow, IMapper<Concert, global::BLL.DTO.Concerts.Concert> mapper) : base(uow.ConcertRepository, mapper)
+    public ConcertService(IAppUOW uow, IMapper<DomainConcert, BLLConcert> mapper) : base(uow.ConcertRepository, mapper)
     {
         _uow = uow;
     }
