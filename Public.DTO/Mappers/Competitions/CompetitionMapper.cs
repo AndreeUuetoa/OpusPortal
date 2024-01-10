c9c323744e2685f5f5452cd7b3bf68e4106f977e
Andree Uuetoa <andree.uuetoa@gmail.com>
2024-01-10 12:33:16 +0200

    Mapper for competition DTO

---
+++ Public.DTO/Mappers/Competitions/CompetitionMapper.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using Base.Mappers;
+
+namespace Public.DTO.Mappers.Competitions;
+
+public class CompetitionMapper : BaseMapper<BLL.DTO.Competitions.Competition, DTO.v1._0.Competitions.Competition>
+{
+    public CompetitionMapper(IMapper mapper) : base(mapper)
+    {
+    }
+}
