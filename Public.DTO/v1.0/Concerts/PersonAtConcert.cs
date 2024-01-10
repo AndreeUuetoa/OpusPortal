21bb863659a63570f33cadb8eeac01e03230badd
Andree Uuetoa <andree.uuetoa@gmail.com>
2024-01-10 12:32:44 +0200

    Remove miswritten DTO

--- Public.DTO/v1.0/Concerts/PersonAtConcert.cs
+++
@@ -1,9 +0,0 @@
-﻿using Base.DTO;
-
-namespace Public.DTO.v1._0.Concerts;
-
-public class PersonAtConcert : DTOEntityId
-{
-    public string Name { get; set; } = default!;
-    public string Location { get; set; } = default!;
-}
