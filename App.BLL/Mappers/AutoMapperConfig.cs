0c79c6995f6fc1ca19abfdefe15db87cc79200bf
Andree Uuetoa <andree.uuetoa@gmail.com>
2024-01-10 12:15:38 +0200

    Remove studying logic and contacts elements

--- App.BLL/Mappers/AutoMapperConfig.cs
+++ App.BLL/Mappers/AutoMapperConfig.cs
@@ -1,10 +1,8 @@
 ﻿using AutoMapper;
 using BLL.DTO.Competitions;
 using BLL.DTO.Concerts;
-using BLL.DTO.Contacts;
 using BLL.DTO.Identity;
 using BLL.DTO.Library;
-using BLL.DTO.Studying_logic;
 
 namespace App.BLL.Mappers;
 
@@ -17,8 +15,6 @@ public class AutoMapperConfig : Profile
         CreateMap<Domain.Library.Book, Book>().ReverseMap();
         CreateMap<Domain.Competitions.Competition, Competition>().ReverseMap();
         CreateMap<Domain.Concerts.Concert, Concert>().ReverseMap();
-        CreateMap<Domain.Studying_logic.AppUserOnCurriculum, PersonOnCurriculum>().ReverseMap();
         CreateMap<Domain.Library.BookLentOut, BookLentOut>().ReverseMap();
-        CreateMap<Domain.Studying_logic.AppUserOnSubject, PersonOnSubject>().ReverseMap();
     }
 }
