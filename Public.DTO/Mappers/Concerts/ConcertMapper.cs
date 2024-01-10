013c20a50a8e1a9df0daed94252b1b237e2f401e
Andree Uuetoa <andree.uuetoa@gmail.com>
2024-01-10 15:50:37 +0200

    Create aliases for concert mapper objects

--- Public.DTO/Mappers/Concerts/ConcertMapper.cs
+++ Public.DTO/Mappers/Concerts/ConcertMapper.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
 using Base.Mappers;
+using BLLConcert = BLL.DTO.Concerts.Concert;
+using PublicConcert = Public.DTO.v1._0.Concerts.Concert;
 
 namespace Public.DTO.Mappers.Concerts;
 
-public class ConcertMapper : BaseMapper<BLL.DTO.Concerts.Concert, DTO.v1._0.Concerts.Concert>
+public class ConcertMapper : BaseMapper<BLLConcert, PublicConcert>
 {
     public ConcertMapper(IMapper mapper) : base(mapper)
     {
