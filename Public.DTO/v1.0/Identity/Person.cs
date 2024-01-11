41a5ad178ba05ef5b98fc305a7dad5ed493aaeeb
Andree Uuetoa <andree.uuetoa@gmail.com>
2024-01-11 13:09:49 +0200

    Remove unused Person DTO

--- Public.DTO/v1.0/Identity/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿using Base.DTO;
-
-namespace Public.DTO.v1._0.Identity;
-
-public class Person : DTOEntityId
-{
-    public string FirstName { get; set; } = default!;
-    public string LastName { get; set; } = default!;
-}
