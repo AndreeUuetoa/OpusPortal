8f5bbd58b30e6314ca3c6284af997d5df850b0f4
Andree Uuetoa <andree.uuetoa@gmail.com>
2024-01-11 13:11:48 +0200

    Remove YearReleased field from Book

--- BLL.DTO/Library/Book.cs
+++ BLL.DTO/Library/Book.cs
@@ -6,5 +6,4 @@ public class Book : DTOEntityId
 {
     public string Title { get; set; } = default!;
     public string Authors { get; set; } = default!;
-    public int YearReleased { get; set; }
 }
